01d8dd7e74b34ee6524180d0badf34b27bd147af
bluediu <reycoc2311@gmail.com>
2023-10-19 18:20:31 -0600

    Fix struct exercise use malloc dinamic memory

--- Structs/struct.c
+++ Structs/struct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 typedef struct {
@@ -8,9 +9,15 @@ typedef struct {
 } Persona;
 
 int main() {
-    // Creación del array
-    Persona personas[1000000];
-    for(int i = 0; i < 1000000; i++) {
+    // Creación del array using dynamic memory allocation
+    Persona *personas = (Persona *)malloc(sizeof(Persona) * 1000000);
+    if (!personas) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    int i = 0;
+    for(i = 0; i < 1000000; i++) {
         personas[i].edad = 25;
         personas[i].id = i;
         sprintf(personas[i].nombre, "Josue");
@@ -18,8 +25,7 @@ int main() {
 
     // Medición de tiempo de acceso
     clock_t start_time = clock();
-    for(int i = 0; i < 1000000; i++) {
-        // volatile char nombre[5] = personas[i].nombre;
+    for(i = 0; i < 1000000; i++) {
         volatile int edad = personas[i].edad;
         volatile int id = personas[i].id;
     }
@@ -27,5 +33,6 @@ int main() {
     double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000;
     printf("Tiempo de acceso: %.2f ms\n", elapsed_time);
 
+    free(personas);  // Liberar la memoria asignada
     return 0;
 }
